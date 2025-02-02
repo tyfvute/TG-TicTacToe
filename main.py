4f70db5bafcf5c4b824cb3eb6fc9d4f3ab606669
Kashel42 <vndmlox@gmail.com>
2025-02-02 21:49:44 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-import telebot;
-bot = telebot.TeleBot('');
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-if message.text == "/start":
-    bot.send_message(message.from_user.id, "Привет, если хочешь начать новую игру, напиши /new game.") 
+
